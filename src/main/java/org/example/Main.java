34fe3b444edcc17c0e12059c6ef16fff427ad7e4
Danylo <roorr123578@gmail.com>
2026-01-14 19:19:24 +0100

    Menu

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,37 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hola Git");
+        int opcion = menuPrincipal();
+        System.out.println("Has seleccionado: " + opcion);
+    }
+
+    private static int menuPrincipal() {
+        Scanner sc = new Scanner(System.in);
+        int opcion = -1;
+
+        while (opcion < 0 || opcion > 4) {
+            System.out.println("\n1. Estado");
+            System.out.println("2. Comer");
+            System.out.println("3. Jugar");
+            System.out.println("4. Dormir");
+            System.out.println("----------------");
+            System.out.println("0. Salir");
+            System.out.print("Seleccione una opción del menú: ");
+
+            if (sc.hasNextInt()) {
+                opcion = sc.nextInt();
+                if (opcion < 0 || opcion > 4) {
+                    System.out.println("Error. Has de elejir entre (0-4).");
+                }
+            } else {
+                System.out.println("Error: Tienes que poner una variable tipo int.");
+                sc.nextLine();
+            }
+        }
+        return opcion;
     }
 }
